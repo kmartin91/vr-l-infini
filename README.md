4a430ab6e713bfcddf46d7c529a0651758c120e7
Kevin Martin <coldfuzion@ckoisa.net>
2018-11-20 22:48:38 +0100

    feat: First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## VR-L-Infini
+
+## Technologies
+
+- 🔥  `react` -  client side view components.
+- 🤖  `aframe` -  3d & VR library.
+- 🚄  `parcel` - bundle manager.
+- 😺  `yarn` - dependencies manager.
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import DefaultScene from './shared/component/scenes/defaultScene';
+
+const Main = () => (
+    <DefaultScene
+      modelLink="/models/obj/captur/captur-news2.obj"
+      modelLinkMtl="/models/obj/captur/captur-news2.mtl"
+      modelLinkType="obj"
+      stats
+      debug
+      sky="https://cdn.aframe.io/a-painter/images/sky.jpg"
+      floor="https://cdn.aframe.io/a-painter/images/floor.jpg"
+    />
+  )
+
+export default Main;
